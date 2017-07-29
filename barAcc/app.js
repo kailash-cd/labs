b22a6fc5d3a32cd8de6b438dc0a7611857116a8c
kailash-cd <kailash@chalkdigital.com>
2017-07-29 14:03:51 +0530

    new updates

--- barAcc/app.js
+++ barAcc/app.js
@@ -5,6 +5,66 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope) {
     $scope.foo = 'This is from the controller!';
+    $scope.AppJSON = {
+        homeGallary: [
+            {
+                "title":"Title",
+                "name":"Name",
+                "path":"images/members/face.gif"
+            },{
+                "title":"Title",
+                "name":"Name",
+                "path":"images/members/face.gif"
+            },{
+                "title":"Title",
+                "name":"Name",
+                "path":"images/members/face.gif"
+            },{
+                "title":"Title",
+                "name":"Name",
+                "path":"images/members/face.gif"
+            },{
+                "title":"Title",
+                "name":"Name",
+                "path":"images/members/face.gif"
+            },{
+                "title":"Title",
+                "name":"Name",
+                "path":"images/members/face.gif"
+            },{
+                "title":"Title",
+                "name":"Name",
+                "path":"images/members/face.gif"
+            },{
+                "title":"Title",
+                "name":"Name",
+                "path":"images/members/face.gif"
+            },{
+                "title":"Title",
+                "name":"Name",
+                "path":"images/members/face.gif"
+            }
+        ],
+        homeBanners:[
+            {
+                "path":"images/banner/1.jpg",
+                "title":"no title"
+            },{
+                "path":"images/banner/2.jpg",
+                "title":"no title"
+            },{
+                "path":"images/banner/3.jpg",
+                "title":"no title"
+            },
+        ],
+        "booleans":{
+            "homepage":{
+                "enabled":true
+            },"aboutpage":{
+                "enabled":true
+            },
+        }
+    }
 
     $scope.w3_open = function() {
         document.getElementById("mySidebar").style.display = "block";
