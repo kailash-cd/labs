d4a5a6fd3eb62241c6efbd8f68f5695c4ff390c8
kailash-cd <kailash@chalkdigital.com>
2017-07-29 20:32:15 +0530

    directory

--- barAcc/app.js
+++ barAcc/app.js
@@ -120,7 +120,91 @@ app.controller('myCtrl', function($scope) {
                 "sodowo":"son of daughter of wife of",
                 "contact":"9411167698",
                 "picPath":"images/members/face.gif"
-            },
+            },{
+                "code":"A-1",
+                "name": "Mr. Akshay Latwal",
+                "designation":"Joint Secretary (Press)",
+                "sodowo":"son of daughter of wife of",
+                "contact":"9458182118",
+                "picPath":"images/members/face.gif"
+            },{
+                "code":"A-1",
+                "name": "Mr. Bhuvnesh Joshi ",
+                "designation":"Senior Executive Members",
+                "sodowo":"son of daughter of wife of",
+                "contact":"9411167576 ",
+                "picPath":"images/members/face.gif"
+            },{
+                "code":"A-1",
+                "name": "Miss. Mamta Joshi ",
+                "designation":"Senior Executive Members",
+                "sodowo":"son of daughter of wife of",
+                "contact":"9412976587 ",
+                "picPath":"images/members/face.gif"
+            },{
+                "code":"A-1",
+                "name": "Mr. Ravi Joshi ",
+                "designation":"Senior Executive Members",
+                "sodowo":"son of daughter of wife of",
+                "contact":"9412035907",
+                "picPath":"images/members/face.gif"
+            },{
+                "code":"A-1",
+                "name": "Mr. Saurabh Kumar Pandey ",
+                "designation":"Senior Executive Members",
+                "sodowo":"son of daughter of wife of",
+                "contact":"9412086862",
+                "picPath":"images/members/face.gif"
+            },{
+                "code":"A-1",
+                "name": "Mr. Tarun Praskh Singh Takuli",
+                "designation":"Senior Executive Members",
+                "sodowo":"son of daughter of wife of",
+                "contact":"94129952576 ",
+                "picPath":"images/members/face.gif"
+            },{
+                "code":"A-1",
+                "name": "Mr. Virendra Singh Rathor",
+                "designation":"Senior Executive Members",
+                "sodowo":"son of daughter of wife of",
+                "contact":"9720129323",
+                "picPath":"images/members/face.gif"
+            },{
+                "code":"A-1",
+                "name": "Miss. Lovely Grover",
+                "designation":"Jounior Executive Members (Re. Wo.)",
+                "sodowo":"son of daughter of wife of",
+                "contact":"9756727644",
+                "picPath":"images/members/face.gif"
+            },{
+                "code":"A-1",
+                "name": "Mr. Akram Parvez ",
+                "designation":"Senior Executive Members",
+                "sodowo":"son of daughter of wife of",
+                "contact":"9319691015 ",
+                "picPath":"images/members/face.gif"
+            },{
+                "code":"A-1",
+                "name": "Miss. Anchit Khokhar ",
+                "designation":"Senior Executive Members",
+                "sodowo":"son of daughter of wife of",
+                "contact":"9690485559 ",
+                "picPath":"images/members/face.gif"
+            },{
+                "code":"A-1",
+                "name": "Mr. Manoj K Sharma ",
+                "designation":"Senior Executive Members",
+                "sodowo":"son of daughter of wife of",
+                "contact":"8439883941",
+                "picPath":"images/members/face.gif"
+            },{
+                "code":"A-1",
+                "name": "Mr. Prabhat ",
+                "designation":"Senior Executive Members",
+                "sodowo":"son of daughter of wife of",
+                "contact":"9412086862 ",
+                "picPath":"images/members/face.gif"
+            }
         ]
     }
 
